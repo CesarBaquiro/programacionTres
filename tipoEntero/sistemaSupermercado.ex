9e63531b0259e2e6c3f5c68ab8f2b63e099509f0
cesarbaquiro <cmbm.kf@gmail.com>
2025-02-25 19:15:51 -0500

    Version 2

--- tipoEntero/sistemaSupermercado.ex
+++ tipoEntero/sistemaSupermercado.ex
@@ -2,21 +2,15 @@ defmodule EntradaEnteros do
   def main do
     valor_total =
       "Ingrese valor total de la factura: "
-      |> ingresar(:entero)
+      |> Util.ingresar(:entero)
 
     valor_entregado =
       "Ingrese valor entregado para el pago: "
-      |> ingresar(:entero)
+      |> Util.ingresar(:entero)
 
     calcular_devuelta(valor_entregado, valor_total)
     |> generar_mensaje()
     |> Util.mostrar_mensaje()
-  end
-
-  defp ingresar(mensaje, :entero) do
-    mensaje
-    |> Util.ingresar(:texto)
-    |> String.to_integer()
   end
 
   defp calcular_devuelta(valor_pago, valor_total) do
